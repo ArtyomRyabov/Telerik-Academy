5a786ead5ef4067a2dbd7498aa649d62cddfa9e1
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-12 23:56:24 +0200

    .init

---
+++ C#Part1/01. Intro-Programming-Homework/08. SquareRoot/SquareRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        // Create a console application that calculates and prints the square root of the number 12345.
+        // Find in Internet “how to calculate square root in C#”.
+
+        double number = 12345;
+        Console.WriteLine("The square root of \"12345\" is: {0}", Math.Sqrt(number));
+    }
+}
+
