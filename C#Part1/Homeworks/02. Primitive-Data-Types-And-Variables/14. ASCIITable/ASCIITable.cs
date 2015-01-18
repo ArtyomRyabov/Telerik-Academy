27b81dab405bb9289891d805044d2bb29783e01f
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-18 21:51:57 +0200

    Homework

--- C#Part1/Homeworks/02. Primitive-Data-Types-And-Variables/14. ASCIITable/ASCIITable.cs
+++ C#Part1/Homeworks/02. Primitive-Data-Types-And-Variables/14. ASCIITable/ASCIITable.cs
@@ -15,7 +15,7 @@ class ASCIITable
         for (int i = 0; i <= 255; i++)
         {
             char symbol = (char)i;
-            Console.WriteLine("Number " + i + " --> " + symbol);
+            Console.WriteLine("Number {0} --> {1}", i, symbol);
         }
     }
 }
