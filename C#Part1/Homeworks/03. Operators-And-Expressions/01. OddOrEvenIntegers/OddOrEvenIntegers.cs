aa3a79e491f27087e1fe66750155eacec5fda7b2
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-19 22:26:01 +0200

    Homework

---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/01. OddOrEvenIntegers/OddOrEvenIntegers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class OddOrEvenIntegers
+{
+    static void Main()
+    {
+        // Write an expression that checks if given integer is odd or even.
+
+        Console.WriteLine("Enter an integer:");
+        int entered = int.Parse(Console.ReadLine());
+
+        if (entered % 2 == 0)
+        {
+            Console.WriteLine("The integer is even.");
+        }
+        else
+        {
+            Console.WriteLine("The integer is odd.");
+        }
+
+        // another solution:
+        Console.WriteLine();
+        bool check = (entered % 2 != 0);
+        Console.WriteLine("This is an odd integer: " + check);
+    }
+}
+
---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/02. GravitationOnTheMoon/GravitationOnTheMoon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class GravitationOnTheMoon
+{
+    static void Main()
+    {
+        // The gravitational field of the Moon is approximately 17% of that on the Earth.
+        // Write a program that calculates the weight of a man on the moon by a given weight on the Earth.
+
+        Console.Write("Enter weight of a man on Earth: ");
+        float weight = float.Parse(Console.ReadLine());
+        float weightDifference = 0.17f;
+        float weightOnMoon = weight * weightDifference;
+        Console.WriteLine("That weight on the Moon will be: {0}", weightOnMoon);
+    }
+}
+
