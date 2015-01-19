5aba4533d46fa0933e9cc2225127b79495d1326f
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-19 23:33:46 +0200

    Homework

--- C#Part1/Homeworks/03. Operators-And-Expressions/03. DivideBy7And5/DivideBy7And5.cs
+++ C#Part1/Homeworks/03. Operators-And-Expressions/03. DivideBy7And5/DivideBy7And5.cs
@@ -11,7 +11,6 @@ class DivideBy7And5
         int number = int.Parse(Console.ReadLine());
         bool check = (number % 5 == 0) && (number % 7 == 0);
         Console.WriteLine("{0} can be divided (without remainder) by 7 and 5 at the same time: {1}", number, check);
-
     }
 }
 
---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/04. RectanglePerimeterArea/RectanglePerimeterArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class RectanglePerimeterArea
+{
+    static void Main()
+    {
+        // Write an expression that calculates rectangle’s perimeter and area by given width and height.
+
+        Console.Write("Enter rectangle's width: ");
+        double width = double.Parse(Console.ReadLine());
+        Console.Write("Enter rectangle's height: ");
+        double height = double.Parse(Console.ReadLine());
+        double perimeter = (width + height) * 2;
+        double area = width * height;
+        Console.WriteLine("Rectangle's perimeter: {0}\nRectangle's area: {1}", perimeter, area);
+    }
+}
+
