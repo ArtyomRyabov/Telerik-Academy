fd38be237e5f4e79411164e0d4e45d32b6362bb5
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-20 15:47:45 +0200

    Homework

---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/05. ThirdDigit7/ThirdDigit7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ThirdDigit7
+{
+    static void Main()
+    {
+        // Write an expression that checks for given integer if its third digit from right-to-left is 7.
+        
+        Console.Write("Enter an integer: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool check = (number % 1000) / 100 == 7;
+        Console.WriteLine("Third digit from right-to-left is 7? \n{0}", check);
+    }
+}
+
+
