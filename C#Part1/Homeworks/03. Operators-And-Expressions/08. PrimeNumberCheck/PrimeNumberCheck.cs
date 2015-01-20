ada0e2327c43e899974a1ff6e59e35f7c02b1c64
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-21 01:36:53 +0200

    Homework

---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/08. PrimeNumberCheck/PrimeNumberCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class PrimeNumberCheck
+{
+    static void Main()
+    {
+        // Write an expression that checks if given positive integer number n (n <= 100) is prime (i.e. it is 
+        // divisible without remainder only to itself and 1).
+
+        Console.Write("Enter positive integer number n (n <= 100): ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("This integer is prime: ");
+        bool oddNumber = true;
+
+        if ((n >= 0) && (n <= 100))
+        {
+            if (n >= 2)
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    if ((n % i == 0) && (n != i))
+                    {
+                        oddNumber = false;
+                    }
+                }
+                Console.WriteLine(oddNumber);
+            }
+            else if (n < 2)
+            {
+                Console.WriteLine(!oddNumber);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Number out of range!");
+        }
+    }
+}
+
