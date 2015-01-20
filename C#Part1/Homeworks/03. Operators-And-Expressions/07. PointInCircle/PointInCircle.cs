35bb9955dbc2da69aec3293552437e7aee459630
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-20 20:22:08 +0200

    Homework

---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/07. PointInCircle/PointInCircle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Write an expression that checks if given point (x, y) is inside a circle K({0, 0}, 2).
+
+        Console.WriteLine("Enter (x, y) coordinates of a point:");
+        Console.Write("x = ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("y = ");
+        double y = double.Parse(Console.ReadLine());
+
+        double radius = 2;
+        bool result = Math.Pow(x, 2) + Math.Pow(y, 2) <= Math.Pow(radius, 2);
+        Console.WriteLine("The point is in the circle: {0}", result);
+    }
+}
+
