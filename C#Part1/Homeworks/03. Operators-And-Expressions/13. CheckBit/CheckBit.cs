bcb16a2d53c8147f6a9b4b2d3c2bee0c31f2d9ea
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-21 16:46:50 +0200

    Homework

---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/13. CheckBit/CheckBit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class CheckBit
+{
+    static void Main()
+    {
+        // Write a Boolean expression that returns if the bit at position p (counting from 0, 
+        // starting from the right) in given integer number n, has value of 1.
+
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+        Console.Write("Enter checked bit's position: ");
+        int p = int.Parse(Console.ReadLine());
+        int mask = 1 << p;
+        bool isZero = (mask & n) == 0;
+        Console.WriteLine("The bit at position {0} is '1': {1}", p, !(isZero));
+    }
+}
+
---
+++ C#Part1/Homeworks/03. Operators-And-Expressions/14. ModifyBit/ModifyBit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class ModifyBit
+{
+    static void Main()
+    {
+        // We are given an integer number n, a bit value v (v=0 or 1) and a position p.
+        // Write a sequence of operators (a few lines of C# code) that modifies n to hold the value v at 
+        // the position p from the binary representation of n while preserving all other bits in n.
+
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+        Console.Write("Enter checked bit's position: ");
+        int p = int.Parse(Console.ReadLine());
+        Console.Write("Enter bit value (0 or 1) to hold on position 'p': ");
+        int v = int.Parse(Console.ReadLine());
+        int mask = 1 << p;
+        int maskNumber;
+
+        if (v == 0)
+        {
+            maskNumber = n & ~(mask);
+        }
+        else 
+        {
+            maskNumber = n | mask;
+        }
+        Console.WriteLine("Result: {0}", maskNumber);
+        Console.WriteLine("Binary result:\n{0}", Convert.ToString(maskNumber, 2).PadLeft(32, '0'));
+    }
+}
+
