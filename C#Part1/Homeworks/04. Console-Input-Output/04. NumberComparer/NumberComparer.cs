8607ec367b339963d54c40e4a3f26064ddcbf10d
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-24 16:36:58 +0200

    Homework

--- C#Part1/Homeworks/04. Console-Input-Output/04. NumberComparer/NumberComparer.cs
+++ C#Part1/Homeworks/04. Console-Input-Output/04. NumberComparer/NumberComparer.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _04.NumberComparer
+class NumberComparer
 {
-    class NumberComparer
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        // Write a program that gets two numbers from the console and prints the greater of them.
+        // Try to implement this without if statements.
+
+        Console.WriteLine("Enter the first number: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine("The greater number is: {0}", Math.Max(a, b));
     }
 }
+
