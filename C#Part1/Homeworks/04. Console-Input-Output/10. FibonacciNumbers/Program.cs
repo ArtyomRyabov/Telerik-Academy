537f2732eefaea593e265510e81bf259b527e660
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-24 21:30:50 +0200

    Homework

--- C#Part1/Homeworks/04. Console-Input-Output/10. FibonacciNumbers/Program.cs
+++ C#Part1/Homeworks/04. Console-Input-Output/10. FibonacciNumbers/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _10.FibonacciNumbers
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Write a program that reads a number n and prints on the console the first n members of 
+        // the Fibonacci sequence (at a single line, separated by comma and 
+        // space - ,) : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ….
+        // Note: You may need to learn how to use loops.
+
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+        int aNumber = 0;
+        int bNumber = 1;
+        int sumAB;
+
+        for (int i = 0; i < n; i++)
         {
+
+            Console.Write("{0}, ", aNumber);
+            sumAB = aNumber + bNumber;
+            aNumber = bNumber;
+            bNumber = sumAB;
         }
+        Console.WriteLine();
     }
 }
+
