36b7085b3c4eabd4a4f77070edf5989c9d3cb022
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-25 17:28:16 +0200

    Homework

--- C#Part1/Homeworks/04. Console-Input-Output/07. SumFiveNumbers/SumFiveNumbers.cs
+++ C#Part1/Homeworks/04. Console-Input-Output/07. SumFiveNumbers/SumFiveNumbers.cs
@@ -6,16 +6,32 @@ class SumFiveNumbers
     {
         // Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
 
+        // solution 1:
+
         Console.Write("Enter 5 numbers in a single line, separated by a space: ");
         string numbers = Console.ReadLine();
-        string[] splitNumbers = numbers.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-        double sum = 0;
-
-        for (int i = 0; i < splitNumbers.Length; i++)
-        {
-            sum += double.Parse(splitNumbers[i]);
-        }
+        string[] splitNumbers = numbers.Split(' ');
+        int firstNumber = int.Parse(splitNumbers[0]);
+        int secondNumber = int.Parse(splitNumbers[1]);
+        int thirdNumber = int.Parse(splitNumbers[2]);
+        int fourthNumber = int.Parse(splitNumbers[3]);
+        int fifthNumber = int.Parse(splitNumbers[4]);
+        int sum = (firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber);
         Console.WriteLine("Sum: {0}", sum);
+
+        // solution 2:
+
+        Console.Write("Enter 5 numbers in a single line, separated by a space: ");
+        string numbs = Console.ReadLine();
+        string[] splitNumbs = numbs.Split(' ');
+
+        double sumNumbs = 0;
+
+        for (int i = 0; i < splitNumbs.Length; i++)
+        {
+            sumNumbs += double.Parse(splitNumbs[i]);
+        }
+        Console.WriteLine("Sum: {0}", sumNumbs);
     }
 }
 
