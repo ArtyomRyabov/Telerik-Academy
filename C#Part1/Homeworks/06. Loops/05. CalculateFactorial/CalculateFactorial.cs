f1771a32bb651ca1611f6cce7a2a0acc700ee187
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-28 19:36:42 +0200

    Homework

---
+++ C#Part1/Homeworks/06. Loops/05. CalculateFactorial/CalculateFactorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class CalculateFactorial
+{
+    static void Main()
+    {
+        // Write a program that, for a given two integer numbers n and x, calculates 
+        // the sum S = 1 + 1!/x + 2!/x^2 + … + n!/x^n.
+        // Use only one loop. Print the result with 5 digits after the decimal point.
+
+        Console.WriteLine("Enter integer n (n > 0):");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter integer x:");
+        int x = int.Parse(Console.ReadLine());
+
+        decimal sum = 1m;
+        long factorial = 1L;
+        long xPow = 1L;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+            xPow *= x;
+            sum += ((decimal)factorial / xPow);
+        }
+        Console.WriteLine("{0:F5}", sum);
+    }
+}
+
