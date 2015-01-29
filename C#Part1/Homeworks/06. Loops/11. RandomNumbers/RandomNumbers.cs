5fdc50d7ddc5e4d1f8401e2579d1ebc1d8e614e1
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-01-29 16:39:57 +0200

    Homework

---
+++ C#Part1/Homeworks/06. Loops/11. RandomNumbers/RandomNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class RandomNumbers
+{
+    static void Main()
+    {
+        // Write a program that enters 3 integers n, min and max (min != max) and prints n random 
+        // numbers in the range [min...max].
+
+        Console.Write("Number of integers to be entered: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter min integer: ");
+        int min = int.Parse(Console.ReadLine());
+        Console.Write("Enter max integer (max > min): ");
+        int max = int.Parse(Console.ReadLine());
+        Random numbers = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(numbers.Next(min, max + 1) + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ C#Part1/Homeworks/06. Loops/12. RandomizedNumbers/RandomizedNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class RandomizedNumbers
+{
+    static void Main()
+    {
+        // Write a program that enters in integer n and prints the numbers 1, 2, …, n in random order.
+        // You might need to use arrays.
+
+        Console.Write("Enter an integer: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        bool[] printed = new bool[n + 1];
+        Random r = new Random();
+
+        for (int i = 1; i <= n; i++)
+        {
+            int numbers = r.Next(1, n + 1);
+            if (printed[numbers])
+            {
+                i--;
+            }
+            else
+            {
+                Console.Write(numbers + " ");
+                printed[numbers] = true;
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
