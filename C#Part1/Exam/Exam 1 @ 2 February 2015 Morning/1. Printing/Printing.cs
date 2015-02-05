8e3c4279cfd566dc33674aa1e4fd3bde142a3c4f
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-05 23:31:37 +0200

    Exam

--- C#Part1/Exam/Exam 1 @ 2 February 2015 Morning/1. Printing/Printing.cs
+++ C#Part1/Exam/Exam 1 @ 2 February 2015 Morning/1. Printing/Printing.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _1.Printing
+class Printing
 {
-    class Printing
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        int n = int.Parse(Console.ReadLine());
+        int s = int.Parse(Console.ReadLine());
+        decimal p = decimal.Parse(Console.ReadLine());
+
+        int allPrintedSheets = n * s;
+        decimal reams = allPrintedSheets / 500m;
+        decimal oneReamMoney = p * 0.45m;
+        decimal savedMoney = reams * p;
+        Console.WriteLine("{0:0.00}", savedMoney);
     }
 }
+
