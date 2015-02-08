acb381ca3b0862715f81150a2ebf2fe03ff27cd9
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-08 12:28:46 +0200

    Homework

---
+++ C#Part2/Homeworks/01. Arrays/10. FindSumInArray/FindSumInArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FindSumInArray
+{
+    static void Main()
+    {
+        // Write a program that finds in given array of integers a sequence of given sum S (if present).
+
+        Console.WriteLine("Enter number for 'sum':");
+        int S = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter elements' number:");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the elements:");
+        int[] array = new int[number];
+        int sum = 0;
+        List<int> sumList = new List<int>();
+
+        for (int i = 0; i < number; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Sequences in the array with sum = {0}:", S);
+
+        for (int i = 0; i < number; i++)
+        {
+            for (int j = i; j < number; j++)
+            {
+                sumList.Add(array[j]);
+                sum += array[j];
+
+                if ((sum == S) && (sumList.Count > 1))
+                {
+                    Console.WriteLine(string.Join(", ", sumList));
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            sum = 0;
+            sumList.Clear();
+        }
+    }
+}
+
