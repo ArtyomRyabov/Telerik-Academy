15cb6c81e22a0a674cc625c4adffe19e9a10f2b8
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-09 17:38:21 +0200

    Homework

---
+++ C#Part2/Homeworks/01. Arrays/15. PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class PrimeNumbers
+{
+    static void Main()
+    {
+        // Write a program that finds all prime numbers in the range [1...10 000 000]. Use the Sieve of Eratosthenes algorithm.
+
+        long n = 10000000;
+        bool[] e = new bool[n];
+        for (int i = 2; i < n; i++)
+        {
+            e[i] = true;
+        }
+        for (int i = 2; i < n; i++)
+        {
+            if (e[i] == true)
+            {
+                for (int j = 2; (j * i) < n; j++)
+                {
+                    e[i * j] = false;
+                }
+            }
+        }
+        for (int i = 0; i < n / 1000; i++)
+        {
+            if (e[i])
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }
+}
+
