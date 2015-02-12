e6fb12e24f0967f289652485cc84bbcfd90b4abb
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-12 17:39:06 +0200

    Homework

--- C#Part2/Homeworks/02. Multidimensional-Arrays/01. FillTheMatrix/FillTheMatrix.cs
+++ C#Part2/Homeworks/02. Multidimensional-Arrays/01. FillTheMatrix/FillTheMatrix.cs
@@ -77,7 +77,7 @@ class FillTheMatrix
             }
             else
             {
-                for (int row = (matrix.GetLength(0) - 1); row >= 0 ; row--)
+                for (int row = (matrix.GetLength(0) - 1); row >= 0; row--)
                 {
                     matrix[row, col] = number++;
                 }
@@ -159,49 +159,46 @@ class FillTheMatrix
 
         // d)
         int[,] newMatrix = new int[n, n];
-        
+
         int newRow = 0;
         int newCol = 0;
         number = 1;
 
-        while (number <= n * n)
+        do
         {
             while ((newRow < n) && (newMatrix[newRow, newCol] == 0))
             {
-                newMatrix[newRow, newCol] = number;
+                newMatrix[newRow, newCol] = number++;
                 newRow++;
-                number++;
             }
             newRow--;
             newCol++;
 
             while ((newCol < n) && (newMatrix[newRow, newCol] == 0))
             {
-                newMatrix[newRow, newCol] = number;
+                newMatrix[newRow, newCol] = number++;
                 newCol++;
-                number++;
             }
             newCol--;
             newRow--;
 
             while ((newRow > -1) && (newMatrix[newRow, newCol] == 0))
             {
-                newMatrix[newRow, newCol] = number;
+                newMatrix[newRow, newCol] = number++;
                 newRow--;
-                number++;
             }
             newRow++;
             newCol--;
 
             while ((newCol >= 0) && (newMatrix[newRow, newCol] == 0))
             {
-                newMatrix[newRow, newCol] = number;
+                newMatrix[newRow, newCol] = number++;
                 newCol--;
-                number++;
             }
             newCol++;
             newRow++;
-        }
+
+        } while (number <= n * n);
 
         Console.WriteLine("This is the matrix according to fourth condition:");
 
