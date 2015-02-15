c79ffe4fe18720556b124bdeca3664c56da6045d
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-15 16:11:54 +0200

    Homework

---
+++ C#Part2/Homeworks/03. Methods/01. SayHello/SayHello.cs
@@ -0,0 +1,42 @@
+﻿// Write a method that asks the user for his name and prints “Hello, <name>”
+// Write a program to test this method.
+
+using System;
+
+class SayHello
+{
+    static void Main()
+    {
+        Greeting();
+    }
+
+    static void Greeting()
+    {
+        Console.Write("What is your name: ");
+        string name = Console.ReadLine();
+        bool isCorrectName = CorrectName(name);
+
+        if (isCorrectName)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+        else
+        {
+            Console.WriteLine("This is not your name!");
+        }
+    }
+
+    static bool CorrectName(string name)
+    {
+        foreach (char item in name)
+        {
+            if ((((int)item > 90 && (int)item < 97)) || ((int)item > 120 || ((int)item < 65) && (item != 32)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
