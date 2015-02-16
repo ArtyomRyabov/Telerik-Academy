62eba2b7132937885c0bb8832ded8b7f5f3a2a62
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-16 09:31:30 +0200

    Homework

---
+++ C#Part2/Homeworks/03. Methods/07. ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,30 @@
+﻿// Write a method that reverses the digits of given decimal number.
+
+using System;
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a decimal number:");
+        decimal number = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine(ReversedecimalNumber(number)); 
+    }
+
+    private static decimal ReversedecimalNumber(decimal number)
+    {
+        char[] array = number.ToString().ToCharArray();
+        char[] reversedArray = new char[array.Length];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            reversedArray[array.Length - 1 - i] = array[i];
+        }
+
+        string reversedString = new string(reversedArray);
+        decimal reversedDecimal = decimal.Parse(reversedString);
+        return reversedDecimal;
+    }
+}
+
