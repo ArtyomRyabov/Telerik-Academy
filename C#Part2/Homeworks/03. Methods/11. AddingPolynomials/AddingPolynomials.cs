37c521db5e8edea0c3a9ac7a430854bcef9b9a21
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-17 11:05:18 +0200

    Homework

---
+++ C#Part2/Homeworks/03. Methods/11. AddingPolynomials/AddingPolynomials.cs
@@ -0,0 +1,124 @@
+﻿// Write a method that adds two polynomials. Represent them as arrays of their coefficients.
+
+using System;
+
+class AddPolynomials
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number of coefficients for the first polynomial:");
+        int firstNumC = int.Parse(Console.ReadLine());
+        decimal[] firstPolynomial = new decimal[firstNumC];
+
+        Console.WriteLine("Enter the coefficients:");
+        WritePolynomial(firstPolynomial);
+
+        Console.WriteLine(new string('-', 50));
+        Console.Write("First polinomial: ");
+        PrintPolynomial(firstPolynomial);
+        Console.WriteLine(new string('-', 50));
+
+        decimal[] reversedFirstArray = new decimal[firstPolynomial.Length];
+        for (int i = 0; i < reversedFirstArray.Length; i++)
+        {
+            reversedFirstArray[i] = firstPolynomial[firstPolynomial.Length - 1 - i];
+        }
+        Console.WriteLine("Reversed first polynomial coefficients: {0}", string.Join(", ", reversedFirstArray));
+
+        Console.WriteLine("Enter number of coefficients for the second polynomial:");
+        int secondNumC = int.Parse(Console.ReadLine());
+        decimal[] secondPolynomial = new decimal[secondNumC];
+
+        Console.WriteLine("Enter the coefficients:");
+        WritePolynomial(secondPolynomial);
+
+        Console.WriteLine(new string('-', 50));
+        Console.Write("Second polinomial: ");
+        PrintPolynomial(secondPolynomial);
+        Console.WriteLine(new string('-', 50));
+
+        decimal[] reversedSecondArray = new decimal[secondPolynomial.Length];
+        for (int i = 0; i < reversedSecondArray.Length; i++)
+        {
+            reversedSecondArray[i] = secondPolynomial[secondPolynomial.Length - 1 - i];
+        }
+        Console.WriteLine("Reversed second polynomial coefficients: {0}", string.Join(", ", reversedSecondArray));
+
+
+        int usedLength = 0;
+
+        if (firstPolynomial.Length >= secondPolynomial.Length)
+        {
+            usedLength = firstPolynomial.Length;
+        }
+        else
+        {
+            usedLength = secondPolynomial.Length;
+        }
+
+        PolynomialsSum(reversedFirstArray, reversedSecondArray, usedLength);
+        Console.WriteLine(new string('-', 50));
+    }
+
+    private static void PolynomialsSum(decimal[] reversedFirstArray, decimal[] reversedSecondArray, int usedLength)
+    {
+        decimal[] sum = new decimal[usedLength];
+
+        for (int i = 0; i < usedLength; i++)
+        {
+            if (i > reversedFirstArray.Length - 1)
+            {
+                sum[i] = reversedSecondArray[i];
+            }
+            else if (i > reversedSecondArray.Length - 1)
+            {
+                sum[i] = reversedFirstArray[i];
+            }
+            else
+            {
+                sum[i] = reversedFirstArray[i] + reversedSecondArray[i];
+            }
+        }
+
+        decimal[] reversedSum = new decimal[usedLength];
+
+        for (int i = 0; i < usedLength; i++)
+        {
+            reversedSum[usedLength - i - 1] = sum[i];
+        }
+
+        Console.WriteLine(new string('-', 50));
+        Console.Write("Polynomials' sum: ");
+        PrintPolynomial(reversedSum);
+    }
+
+    private static void PrintPolynomial(decimal[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i == array.Length - 1)
+            {
+                Console.Write("{0}", array[i]);
+            }
+            else if (i == array.Length - 2)
+            {
+                Console.Write("{0}x + ", array[i]);
+            }
+            else
+            {
+                Console.Write("{0}x^{1} + ", array[i], (array.Length - 1 - i));
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void WritePolynomial(decimal[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = decimal.Parse(Console.ReadLine());
+        }
+    }
+}
+
---
+++ C#Part2/Homeworks/03. Methods/12. SubtractingPolynomials/SubtractingPolynomials.cs
@@ -0,0 +1,144 @@
+﻿// Extend the previous program to support also substraction and multiplication of polynomials.
+
+using System;
+
+class SubtractingPolynomials
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter number of coefficients for the first polynomial:");
+        int firstCoefNum = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the coefficients reversed way:");
+        decimal[] firstPolynomial = new decimal[firstCoefNum];
+        WritePolynomial(firstPolynomial);
+        Console.WriteLine(new string('-', 50));
+        Console.Write("First Polynomial: ");
+        PrintPolynomial(firstPolynomial);
+        Console.WriteLine(new string('-', 50));
+
+        Console.WriteLine("Enter number of coefficients for the second polynomial:");
+        int secondCoefNum = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the coefficients reversed way:");
+        decimal[] secondPolynomial = new decimal[secondCoefNum];
+        WritePolynomial(secondPolynomial);
+        Console.WriteLine(new string('-', 50));
+        Console.Write("Second Polynomial: ");
+        PrintPolynomial(secondPolynomial);
+        Console.WriteLine(new string('-', 50));
+
+        decimal[] substractPolynomials = new decimal[MaxLength(firstPolynomial, secondPolynomial)];
+        Substract(firstPolynomial, secondPolynomial, substractPolynomials);
+        Console.WriteLine(new string('-', 50));
+        Console.Write("Substract polynomials: ");
+        PrintPolynomial(substractPolynomials);
+        Console.WriteLine(new string('-', 50));
+
+        decimal[] multiplyPolynomials = new decimal[firstPolynomial.Length + secondPolynomial.Length];
+        Multiply(firstPolynomial, secondPolynomial, multiplyPolynomials);
+        Console.WriteLine(new string('-', 50));
+        Console.Write("Multiply polynomials: ");
+        PrintPolynomial(multiplyPolynomials);
+        Console.WriteLine(new string('-', 50));
+    }
+
+    private static void WritePolynomial(decimal[] polynomial)
+    {
+        for (int i = 0; i < polynomial.Length; i++)
+        {
+            polynomial[i] = decimal.Parse(Console.ReadLine());
+        }
+    }
+
+    static void PrintPolynomial(decimal[] polynomial)
+    {
+        for (int i = polynomial.Length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine("{0}", polynomial[i]);
+            }
+            else if (i == 1)
+            {
+                if (polynomial[i - 1] >= 0)
+                {
+                    Console.Write("{0}x + ", polynomial[i]);
+                }
+                else
+                {
+                    Console.Write("{0}x ", polynomial[i]);
+                }
+            }
+            else if (polynomial[i] != 0 && i > 1)
+            {
+                if (polynomial[i - 1] >= 0)
+                {
+                    Console.Write("{0}x^{1} + ", polynomial[i], i);
+                }
+                else
+                {
+                    Console.Write("{0}x^{1} ", polynomial[i], i);
+                }
+            }
+        }
+    }
+
+    private static int MaxLength(decimal[] firstPolynomial, decimal[] secondPolynomial)
+    {
+        int maxLength = 0;
+
+        if (firstPolynomial.Length > secondPolynomial.Length)
+        {
+            maxLength = firstPolynomial.Length;
+        }
+        else
+        {
+            maxLength = secondPolynomial.Length;
+        }
+
+        return maxLength;
+    }
+
+    static void Substract(decimal[] firstPolynomial, decimal[] secondPolynomial, decimal[] substractPolynomials)
+    {
+        int minLenght = 0;
+
+        if (firstPolynomial.Length > secondPolynomial.Length)
+        {
+            minLenght = secondPolynomial.Length;
+        }
+        else
+        {
+            minLenght = firstPolynomial.Length;
+        }
+
+        for (int i = 0; i < minLenght; i++)
+        {
+            substractPolynomials[i] = firstPolynomial[i] - secondPolynomial[i];
+        }
+
+        for (int i = minLenght; i < substractPolynomials.Length; i++)
+        {
+            if (minLenght == firstPolynomial.Length)
+            {
+                substractPolynomials[i] = -secondPolynomial[i];
+            }
+            else
+            {
+                substractPolynomials[i] = firstPolynomial[i];
+            }
+        }
+    }
+
+    static void Multiply(decimal[] firstPolynomial, decimal[] secondPolynomial, decimal[] multiplyPolynomials)
+    {
+        for (int i = 0; i < firstPolynomial.Length; i++)
+        {
+            for (int j = 0; j < secondPolynomial.Length; j++)
+            {
+                int index = i + j;
+                multiplyPolynomials[index] += (firstPolynomial[i] * secondPolynomial[j]);
+            }
+        }
+    }
+}
+
