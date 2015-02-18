6203465d554fab412aac94a3f09fa5db3354754e
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-18 13:30:45 +0200

    Homework

---
+++ C#Part2/Homeworks/04. Numeral-Systems/01. DecimalToBinary/DecimalToBinary.cs
@@ -0,0 +1,29 @@
+﻿// Write a program to convert decimal numbers to their binary representation.
+
+using System;
+
+class DecimalToBinary
+{
+    static void Main()
+    {
+        Console.Write("Enter a decimal number: ");
+        long decimalNumber = long.Parse(Console.ReadLine());
+
+        Console.WriteLine("Converted to binary: {0}", DecimalToBinaryNumber(decimalNumber));
+    }
+
+    private static string DecimalToBinaryNumber(long decimalNumber)
+    {
+        string binaryNumber = string.Empty;
+
+        while (decimalNumber > 0)
+        {
+            byte remainder = (byte)(decimalNumber % 2);
+            decimalNumber /= 2;
+            binaryNumber = remainder + binaryNumber;
+        }
+
+        return binaryNumber;
+    }
+}
+
---
+++ C#Part2/Homeworks/04. Numeral-Systems/02. BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,28 @@
+﻿// Write a program to convert binary numbers to their decimal representation.
+
+using System;
+
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        Console.Write("Enter a binary number: ");
+        string binaryNumber = Console.ReadLine();
+
+        Console.WriteLine("Converted to decimal: {0}", BinaryToDecimalNumber(binaryNumber));
+    }
+
+    private static long BinaryToDecimalNumber(string binaryNumber)
+    {
+        long decimalNumber = 0;
+
+        for (int i = binaryNumber.Length - 1; i >= 0; i--)
+        {
+            long multiplier = (long)Math.Pow(2, (binaryNumber.Length - 1 - i));
+            decimalNumber += (binaryNumber[i] - '0') * multiplier;
+        }
+
+        return decimalNumber;
+    }
+}
+
