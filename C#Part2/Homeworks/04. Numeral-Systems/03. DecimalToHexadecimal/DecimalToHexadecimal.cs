ae73648dc2a95c2e4e995ae54d04ab64ff34f8c6
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-18 16:46:44 +0200

    Homework

---
+++ C#Part2/Homeworks/04. Numeral-Systems/03. DecimalToHexadecimal/DecimalToHexadecimal.cs
@@ -0,0 +1,36 @@
+﻿// Write a program to convert decimal numbers to their hexadecimal representation.
+
+using System;
+
+class DecimalToHexadecimal
+{
+    static void Main()
+    {
+        Console.Write("Enter a decimal number: ");
+        long decimalNumber = long.Parse(Console.ReadLine());
+
+        Console.WriteLine("Converted to hexademical: {0}", DecimalToHexademicalNumber(decimalNumber));
+    }
+
+    private static string DecimalToHexademicalNumber(long decimalNumber)
+    {
+        string hexademicalNumber = string.Empty;
+        while (decimalNumber > 0)
+        {
+            byte remainder = (byte)(decimalNumber % 16);
+            decimalNumber /= 16;
+
+            if ((remainder <= 9) && (remainder >= 0))
+            {
+                hexademicalNumber = (char)(remainder + '0') + hexademicalNumber;
+            }
+            else if ((remainder <= 15) && (remainder >= 10))
+            {
+                hexademicalNumber = (char)(remainder - 10 + 'A') + hexademicalNumber;
+            }  
+        }
+        
+        return hexademicalNumber;
+    }
+}
+
---
+++ C#Part2/Homeworks/04. Numeral-Systems/04. HexadecimalToDecimal/Program.cs
@@ -0,0 +1,36 @@
+﻿// Write a program to convert hexadecimal numbers to their decimal representation.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a hexadecimal number: ");
+        string hexadecimalNumber = Console.ReadLine();
+
+        Console.WriteLine("Converted to decimal: {0}", HexadecimalToDecimalNumber(hexadecimalNumber));
+    }
+
+    private static long HexadecimalToDecimalNumber(string hexadecimalNumber)
+    {
+        long decimalNumber = 0;
+
+        for (int i = hexadecimalNumber.Length - 1; i >= 0; i--)
+        {
+            if ((hexadecimalNumber[i] >= '0') && (hexadecimalNumber[i] <= '9'))
+            {
+                long multiplier = (long)Math.Pow(16, (hexadecimalNumber.Length - 1 - i));
+                decimalNumber += (hexadecimalNumber[i] - '0') * multiplier;
+            }
+            else if ((hexadecimalNumber[i] >= 'A') && (hexadecimalNumber[i] <= 'F'))
+            {
+                long multiplier = (long)Math.Pow(16, (hexadecimalNumber.Length - 1 - i));
+                decimalNumber += (hexadecimalNumber[i] - 'A' + 10) * multiplier;
+            }
+        }
+
+        return decimalNumber;
+    }
+}
+
