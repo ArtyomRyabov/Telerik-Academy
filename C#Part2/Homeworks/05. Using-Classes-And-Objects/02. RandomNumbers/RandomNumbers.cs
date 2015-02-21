1d622f2b8b9f601c9daf4597968df7ec405f327c
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-21 19:05:30 +0200

    Homework

---
+++ C#Part2/Homeworks/05. Using-Classes-And-Objects/02. RandomNumbers/RandomNumbers.cs
@@ -0,0 +1,24 @@
+﻿// Write a program that generates and prints to the console 10 random values in the range [100, 200].
+
+using System;
+
+namespace RandomNumbers
+{
+    class RandomNumbers
+    {
+        static void Main()
+        {
+            Random value = new Random();
+            Console.WriteLine("Ten random values in the range [100, 200]:");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", value.Next(100, 201));
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        }
+    }
+}
