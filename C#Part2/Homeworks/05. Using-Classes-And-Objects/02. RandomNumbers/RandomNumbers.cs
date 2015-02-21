dfc675946c1905258bedd4e6e270a941120984ec
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-21 19:25:17 +0200

    Homework

--- C#Part2/Homeworks/05. Using-Classes-And-Objects/02. RandomNumbers/RandomNumbers.cs
+++ C#Part2/Homeworks/05. Using-Classes-And-Objects/02. RandomNumbers/RandomNumbers.cs
@@ -2,23 +2,21 @@
 
 using System;
 
-namespace RandomNumbers
+class RandomNumbers
 {
-    class RandomNumbers
+    static void Main()
     {
-        static void Main()
+        Random value = new Random();
+        Console.WriteLine("Ten random values in the range [100, 200]:");
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        for (int i = 0; i < 10; i++)
         {
-            Random value = new Random();
-            Console.WriteLine("Ten random values in the range [100, 200]:");
-            Console.ForegroundColor = ConsoleColor.Blue;
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write("{0} ", value.Next(100, 201));
-            }
-
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine();
+            Console.Write("{0} ", value.Next(100, 201));
         }
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine();
     }
 }
+
