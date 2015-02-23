98d1e242589aa03f27fdf04664cdc25d6a0d98c9
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-23 15:44:24 +0200

    Homework

--- C#Part2/Homeworks/06. Strings-And-Text-Processing/03. CorrectBrackets/CorrectBrackets.cs
+++ C#Part2/Homeworks/06. Strings-And-Text-Processing/03. CorrectBrackets/CorrectBrackets.cs
@@ -26,6 +26,11 @@ namespace P03.CorrectBrackets
 
             for (int i = 0; i < input.Length; i++)
             {
+                if (((i > 0) && (input[i] == ')') && (input[i - 1] != ')')) && (input[i - 1] < '0' || input[i - 1] > '9'))
+                {
+                    return false;
+                }
+
                 if (input[i] == '(')
                 {
                     count++;
@@ -33,6 +38,11 @@ namespace P03.CorrectBrackets
                 else if (input[i] == ')')
                 {
                     count--;
+                }
+
+                if (count == -1)
+                {
+                    return false;
                 }
             }
 
@@ -43,7 +53,7 @@ namespace P03.CorrectBrackets
             else
             {
                 return false;
-            }
+            }   
         }
     }
 }
