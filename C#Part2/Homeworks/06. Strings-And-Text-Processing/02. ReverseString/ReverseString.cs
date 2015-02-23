a38457640c0d3792811f7e89ec4eb4df76d07d6c
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-23 14:47:53 +0200

    Homework

---
+++ C#Part2/Homeworks/06. Strings-And-Text-Processing/02. ReverseString/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿// Write a program that reads a string, reverses it and prints the result at the console.
+
+namespace P02.ReverseString
+{
+    using System;
+    using System.Linq;
+
+    class ReverseString
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string to reverse: ");
+            string input = Console.ReadLine();
+
+            string reversed = new string(input.Reverse().ToArray());
+            Console.WriteLine("Reversed: {0}", reversed);
+        }
+    }
+}
---
+++ C#Part2/Homeworks/06. Strings-And-Text-Processing/03. CorrectBrackets/CorrectBrackets.cs
@@ -0,0 +1,49 @@
+﻿// Write a program to check if in a given expression the brackets are put correctly.
+// Example of correct expression: ((a+b)/5-d). Example of incorrect expression: )(a+b)).
+
+namespace P03.CorrectBrackets
+{
+    using System;
+
+    class CorrectBrackets
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter an expression:");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(CheckBrackets(input) ? "The brackets are put correctly!" : "The brackets are put incorrectly!"); 
+        }
+
+        private static bool CheckBrackets(string input)
+        {
+            int count = 0;
+
+            if ((input.IndexOf(')') < input.IndexOf('(')) || (input.LastIndexOf(')') < input.LastIndexOf('(')))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    count++;
+                }
+                else if (input[i] == ')')
+                {
+                    count--;
+                }
+            }
+
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
