ea18ce641d3f00cf634f37acb92af788f6dae869
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-24 19:03:56 +0200

    Homework

---
+++ C#Part2/Homeworks/06. Strings-And-Text-Processing/13. ReverseSentence/ReverseSentence.cs
@@ -0,0 +1,53 @@
+﻿// Write a program that reverses the words in given sentence.
+
+namespace P13.ReverseSentence
+{
+    using System;
+    using System.Text;
+
+    class ReverseSentence
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string text = Console.ReadLine();
+            string[] words = text.Split(new char[] { ' ', ',', '.', '?', '!', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] nonWords = text.Split(words, StringSplitOptions.RemoveEmptyEntries);
+
+            Array.Reverse(words);
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(ReverseTheWords(words, nonWords));
+        }
+
+        private static string ReverseTheWords(string[] words, string[] nonWords)
+        {
+            StringBuilder result = new StringBuilder();
+            int maxLength = 0;
+
+            if (words.Length >= nonWords.Length)
+            {
+                maxLength = words.Length;
+            }
+            else
+            {
+                maxLength = nonWords.Length;
+            }
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < words.Length)
+                {
+                    result.Append(words[i]);
+                }
+
+                if (i < nonWords.Length)
+                {
+                    result.Append(nonWords[i]);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
