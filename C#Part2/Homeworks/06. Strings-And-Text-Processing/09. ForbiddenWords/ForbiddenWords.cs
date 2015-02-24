aa0257fe51df4262f1fccc3c23031b10b7bf1383
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-24 13:48:28 +0200

    Homework

---
+++ C#Part2/Homeworks/06. Strings-And-Text-Processing/09. ForbiddenWords/ForbiddenWords.cs
@@ -0,0 +1,31 @@
+﻿// We are given a string containing a list of forbidden words and a text containing some of these words.
+// Write a program that replaces the forbidden words with asterisks.
+
+namespace P09.ForbiddenWords
+{
+    using System;
+
+    class ForbiddenWords
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the text:");
+            string text = Console.ReadLine().ToLower();
+            Console.WriteLine("\n" + "Enter the forbidden words, separated by space:");
+            string[] words = Console.ReadLine().ToLower().Split(' ', ',', '.');
+
+            Console.WriteLine("\n" + "Result:");
+            Console.WriteLine(ChangeForbiddenWords(text, words));
+        }
+
+        private static string ChangeForbiddenWords(string text, string[] words)
+        {
+            foreach (string word in words)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            return text;
+        }
+    }
+}
