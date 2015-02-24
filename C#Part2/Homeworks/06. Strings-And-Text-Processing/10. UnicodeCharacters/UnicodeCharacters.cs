b15525fda967fb8408b4cc854085e29d4ded60bf
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-24 14:09:35 +0200

    Homework

---
+++ C#Part2/Homeworks/06. Strings-And-Text-Processing/10. UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,25 @@
+﻿// Write a program that converts a string to a sequence of C# Unicode character literals.
+// Use format strings.
+
+namespace P10.UnicodeCharacters
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Result: ");
+
+            foreach (char item in input)
+            {
+                Console.Write("\\u{0:x4}", (int)item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
