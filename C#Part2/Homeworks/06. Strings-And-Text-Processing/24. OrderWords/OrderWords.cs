dd592dae88d11d433455349b8188ea431504236b
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-02-26 16:39:05 +0200

    Homework

---
+++ C#Part2/Homeworks/06. Strings-And-Text-Processing/24. OrderWords/OrderWords.cs
@@ -0,0 +1,20 @@
+﻿// Write a program that reads a list of words, separated by spaces and prints the list in an alphabetical order.
+
+namespace P24.OrderWords
+{
+    using System;
+
+    class OrderWords
+    {
+        static void Main()
+        {
+            string input = "Write a program that reads a list of words separated by " +
+                           "spaces and prints the list in an alphabetical order";
+            string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(words);
+
+            Console.WriteLine("Input:\n" + input);
+            Console.WriteLine("\nOutput:\n{0}\n", string.Join(" ", words));
+        }
+    }
+}
