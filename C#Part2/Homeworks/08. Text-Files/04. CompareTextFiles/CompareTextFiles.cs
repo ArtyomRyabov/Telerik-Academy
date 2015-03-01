61563b82ff0bec0f3bd677a2232d55a89156e713
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-03-01 22:14:56 +0200

    Homework

---
+++ C#Part2/Homeworks/08. Text-Files/04. CompareTextFiles/CompareTextFiles.cs
@@ -0,0 +1,43 @@
+﻿// Write a program that compares two text files line by line and prints the number of lines that are the same and 
+// the number of lines that are different. Assume the files have equal number of lines.
+
+namespace P04.CompareTextFiles
+{
+    using System;
+    using System.IO;
+
+    class CompareTextFiles
+    {
+        static void Main()
+        {
+            using (StreamReader firstReader = new StreamReader(@"..\..\first_file.txt"))
+            {
+                using (StreamReader secondReader = new StreamReader(@"..\..\second_file.txt"))
+                {
+                    string firstFileLine = firstReader.ReadLine();
+                    string secondFileLine = secondReader.ReadLine();
+                    int same = 0;
+                    int different = 0;
+
+                    while (firstFileLine != null)
+                    {
+                        if (firstFileLine.CompareTo(secondFileLine) == 0)
+                        {
+                            same++;
+                        }
+                        else
+                        {
+                            different++;
+                        }
+
+                        firstFileLine = firstReader.ReadLine();
+                        secondFileLine = secondReader.ReadLine();
+                    }
+
+                    Console.WriteLine("The number of same lines is: {0}", same);
+                    Console.WriteLine("The number of different lines is: {0}", different);
+                }
+            }
+        }
+    }
+}
