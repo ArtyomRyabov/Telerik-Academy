b0885772f9c23c45bd857f954f1a2b340669b938
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-03-19 22:09:55 +0200

    Homework

---
+++ Object-Oriented-Programming/Homeworks/03. Extension-Methods-Delegates-Lambda-LINQ/01. StringBuilder.Substring/Test.cs
@@ -0,0 +1,24 @@
+﻿namespace StringBuilder.Extensions
+{
+    using System;
+    using System.Text;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            StringBuilder test = new StringBuilder();
+            test.Append("Testing StringBuilder.Substring");
+            Console.WriteLine(test);
+
+            test = test.Substring(0, 31);    
+            Console.WriteLine(test);               // Must return the original substring
+
+            test = test.Substring(22, 9);
+            Console.WriteLine(test);               // Must return "Substring"
+
+            test = test.Substring(5, 0);       // Must return null
+            Console.WriteLine(test);
+        }
+    }
+}
