7095414d36248b9d22103776fecab7e691af9538
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-03-26 10:13:03 +0200

    Homework

---
+++ Object-Oriented-Programming/Homeworks/05. OOP-Principles-Part-2/1. Shapes/Square.cs
@@ -0,0 +1,22 @@
+﻿namespace Shapes
+{
+    using System;
+
+    public class Square : Shape
+    {
+        public Square(double width)
+            : base(width, width)
+        {
+        }
+
+        public override double CalculateSurface()
+        {
+            return this.Width * this.Width;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} (side = {1}), surface = {2:F2}", this.GetType().Name, this.Width, this.CalculateSurface());
+        }
+    }
+}
