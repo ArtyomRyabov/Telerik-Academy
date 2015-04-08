e90e784411c5c78dc00c4f738ed47483dcb0ad83
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-04-08 19:09:09 +0300

    Exam Problems

---
+++ Object-Oriented-Programming/Exam Problems/CosmeticsShop/Cosmetics/Products/ShoppingCart.cs
@@ -0,0 +1,45 @@
+﻿namespace Cosmetics.Products
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Cosmetics.Common;
+    using Cosmetics.Contracts;
+
+    public class ShoppingCart : IShoppingCart
+    {
+        private const string ProductToAdd = "Product to add";
+        private const string ProductToRemove = "Product to remove";
+
+        private ICollection<IProduct> products;
+
+        public ShoppingCart()
+        {
+            this.products = new List<IProduct>();
+        }
+
+        public void AddProduct(IProduct product)
+        {
+            Validator.CheckIfNull(product, string.Format(GlobalErrorMessages.ObjectCannotBeNull, ProductToAdd));
+
+            this.products.Add(product);
+        }
+
+        public void RemoveProduct(IProduct product)
+        {
+            Validator.CheckIfNull(product, string.Format(GlobalErrorMessages.ObjectCannotBeNull, ProductToRemove));
+
+            this.products.Remove(product);
+        }
+
+        public bool ContainsProduct(IProduct product)
+        {
+            return this.products.Contains(product);
+        }
+
+        public decimal TotalPrice()
+        {
+            return this.products.Sum(p => p.Price);
+        }
+    }
+}
