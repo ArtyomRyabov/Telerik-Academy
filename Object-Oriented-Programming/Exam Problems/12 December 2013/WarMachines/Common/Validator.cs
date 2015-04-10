512f61812535d1fe88dff5c268b75cdb9d14b765
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-04-10 09:39:44 +0300

    Exam Problems

---
+++ Object-Oriented-Programming/Exam Problems/12 December 2013/WarMachines/Common/Validator.cs
@@ -0,0 +1,23 @@
+﻿namespace WarMachines.Common
+{
+    using System;
+
+    public static class Validator
+    {
+        public static void CheckIfNullObject(object obj, string message = null)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException(message);
+            }
+        }
+
+        public static void ChekIfStringIsNullOrEmty(string str, string message = null)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new NullReferenceException(message);
+            }
+        }
+    }
+}
