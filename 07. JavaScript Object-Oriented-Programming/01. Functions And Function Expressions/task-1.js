f41aa07c7d74795801fca81d9fb9fad1db219312
GeorgiJefferski <georgi.jefferski@gmail.com>
2015-06-17 23:50:27 +0300

    Homework

---
+++ 07. JavaScript Object-Oriented-Programming/01. Functions And Function Expressions/task-1.js
@@ -0,0 +1,30 @@
+/* 
+	Write a function that sums an array of numbers:
+		numbers must be always of type Number
+		returns `null` if the array is empty
+		throws Error if the parameter is not passed (undefined)
+		throws if any of the elements is not convertible to Number	
+
+*/
+
+function sum(nums) {
+    if (nums.length === 0) {
+        return null;
+    }
+
+    if (nums === undefined) {
+        throw new Error('Array not passed!');
+    }
+
+    if (nums.some(function(num) {
+            return isNaN(num);
+        })) {
+        throw new Error('Only numbers allowed!');
+    }
+
+    return nums.reduce(function(sum, num) {
+        return sum += (+num);
+    }, 0);
+}
+
+module.exports = sum;
